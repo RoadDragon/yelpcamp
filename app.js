7bab4ce43afa961dcaba5a4f6fd489b7bc5e867d
Leslie Lello <lesliesnewemail@gmail.com>
2017-01-08 13:49:53 +0000

    add mongo lab

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ var commentRoutes       = require("./routes/comments"),
     indexRoutes          = require("./routes/index");
     
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://leslie:buddha@ds031922.mlab.com:31922/yelpcamp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
